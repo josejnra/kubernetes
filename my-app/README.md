b7e32e9c1fa935be292f08ae2041e230b5fe4385
José Nunes <josenunesra@gmail.com>
2021-11-16 23:37:47 -0300

    feat: change db from deployment kind to statefulset

---
+++ my-app/README.md
@@ -0,0 +1,7 @@
+# My App
+Just a dummy app in order to learn kubernetes basics.
+
+## Access App
+```shell
+kubectl port-forward svc/app-service 8080:80
+```
--- my-app/k8s-manifests/install-all-components.sh
+++ my-app/k8s-manifests/install-all-components.sh
@@ -8,7 +8,7 @@ kubectl apply -f db-persistent-volume-claim.yaml
 kubectl apply -f db-secret.yaml
 kubectl apply -f db-configmap.yaml
 
-kubectl apply -f db-deployment.yaml
+kubectl apply -f db-statefulset.yaml
 kubectl apply -f db-service.yaml
 
 kubectl apply -f app-deployment.yaml
